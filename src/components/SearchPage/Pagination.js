abe0a1e1999c7f8bc57e2f2d83344f726994c26f
yingchengchen <yingchengchen08@gmail.com>
2022-06-16 16:26:14 +0800

    feat: 搜尋結果(換頁)

---
+++ src/components/SearchPage/Pagination.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { ChakraProvider,Button,Box } from '@chakra-ui/react'
+  
+const Pagination = ({postsPerPage, totalPosts, paginate}) => {
+    const pageNumbers = [];
+    for(let i=1;i<= Math.ceil(totalPosts/ postsPerPage);i++){
+        pageNumbers.push(i);
+    }
+
+    return (
+        <ChakraProvider>
+                <div >
+                    {pageNumbers.map(number => (
+                        <Box key={number} ml='10'  >
+                            <Button  onClick={()=> paginate(number)} colorScheme='blue' variant='outline' mt='2' >
+                                {number}
+                            </Button>
+                        </Box>
+                    ))}
+                </div>
+            
+        </ChakraProvider>
+        
+    )
+};
+  
+export default Pagination;
